443bb2f4d7652cf0d92f24c005e1d85df9cdc3e2
rmusana <rmusanaproject@gmail.com>
2026-07-30 13:06:44 +0300

    Delete RMUSANA directory

--- RMUSANA/js/config.js
+++
@@ -1,3 +0,0 @@
-window.RMUSANA_API_URL = 'https://script.google.com/macros/s/AKfycbwI5Uhg4gPbfGFMzNeyoew5uURsskoqLdMoi4MUmsND3JlU4wfx1SbrxPebgEFjrSQP/exec';
-window.RMUSANA_GOOGLE_CLIENT_ID = '';
-window.RMUSANA_VERSION = '1.0.0';
